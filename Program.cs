5fa16dad382674467081a83621b0578c55c7622b
Maxime COMINOTTO <cominotto.maxime@gmail.com>
2020-06-08 08:55:45 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Enumerable.Range(1, 100).ToArray();
+
+            foreach (var number in sequence)
+            {
+                Write(number switch
+                {
+                    int i when (number % 3 == 0 && number % 5 == 0) => "FizzBuzz",
+                    int i when (number % 3 == 0) => "Fizz",
+                    int i when (number % 5 == 0) => "Buzz",
+                    _ => number.ToString()
+                });
+                Write(" ");
+            }
+        }
+    }
+}
